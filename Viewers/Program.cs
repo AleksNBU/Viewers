ac4882023e382dd20584dec6e44fa588586ada17
Aleksandr Baranov <aleksaleksand1@gmail.com>
2016-07-14 20:20:28 +0300

    Ready

---
+++ Viewers/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Viewer[,] Cinema = new Viewer[5, 10];
+            Random rnd1 = new Random();
+            for (int i = 0; i < Cinema.GetLength(0); i++)
+            {
+                for (int j = 0; j < Cinema.GetLength(1); j++)
+                {
+                    int x = rnd1.Next(2);
+                    if (x == 0)
+                    {
+                        Cinema[i, j] = null;
+                    }
+                    else Cinema[i, j] = new Viewer();
+                }
+            }
+            int total = 0, totalAge = 0, totalMale = 0, totalFemal = 0, ageMale = 0, ageFemal = 0;
+
+            for (int i = 0; i < Cinema.GetLength(0); i++)
+            {
+                for (int j = 0; j < Cinema.GetLength(1); j++)
+                {
+                    if (Cinema[i, j] != null)
+                    {
+                        total++;
+                        totalAge += Cinema[i, j].Age;
+                        if (Cinema[i, j].Gender.ToString() == "Male")
+                        {
+                            totalMale++;
+                            ageMale += Cinema[i, j].Age;
+                        }
+                        else
+                        {
+                            totalFemal++;
+                            ageFemal += Cinema[i, j].Age;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"In the cinema {total} viewers.");
+            if (total != 0)
+            {
+                Console.WriteLine($"The average age of the viewer {totalAge / total} years.");
+            }
+            else Console.WriteLine($"In the cinema there are no the audience");
+            Console.WriteLine($"The total number of men {totalMale}.");
+            Console.WriteLine($"The total number of women {totalFemal}.");
+            if (totalMale != 0)
+            {
+                Console.WriteLine($"The average age of the men {ageMale / totalMale} years.");
+            }
+            else Console.WriteLine($"In the cinema there are no men.");
+            if (totalFemal != 0)
+            {
+                Console.WriteLine($"The average age of the men {ageFemal / totalFemal} years.");
+            }
+            else Console.WriteLine($"In the cinema there are no women.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Viewers/Viewers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audience
+{
+    class Viewer
+    {
+        private string _name;
+        private string _secondName;
+        private int _age;
+        private Sex _sex;
+        public enum Sex
+        {
+            Male,
+            Female
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string SecondName
+        {
+            get { return _secondName; }
+            set { _secondName = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public Sex Gender
+        {
+            get { return _sex; }
+            set { _sex = value; }
+        }
+
+        string[] arrNameFemale = new string[5] { "Amanda", "Betty", "Ella", "Gloria", "Kristina" };
+        string[] arrNameMale = new string[5] { "Felix", "Conan", "Crosby", "Benjamin", "Albert" };
+        string[] arrSecondName = new string[5] { "CONNER", "DARRELL", "EDISON", "GOODWIN", "HILL" };
+        static Random rnd = new Random();
+        public Viewer()
+        {
+            int mIndex = rnd.Next(0, arrNameMale.Length);
+            int fIndex = rnd.Next(0, arrNameFemale.Length);
+            int snIndex = rnd.Next(0, arrSecondName.Length);
+            int age = rnd.Next(15, 75);
+            Sex gender = (Sex)rnd.Next(2);
+            if (gender.ToString() == "Male")
+            {
+                Name = arrNameMale[mIndex];
+            }
+            else
+            {
+                Name = arrNameFemale[fIndex];
+            }
+            SecondName = arrSecondName[snIndex];
+            Age = age;
+            Gender = gender;
+        }
+    }
+}
